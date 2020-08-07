6e7fa844474a9e99caeeed2c31cf5a615b50c68a
Savantage <llkisamell@gmail.com>
2020-08-07 00:18:14 -0300

    Update finished

--- main/engine/roles/dualist.py
+++ main/engine/roles/dualist.py
@@ -2,9 +2,9 @@ from engine.roles.dualist_stats import *
 from engine.wearable import Wearable
 
 dualist_helmetS = {'Default': Wearable(),
-                   'Legendary': Wearable("Legendary", 2, 3, 3, 3, 2, 8, 10, 0.52, 0.08, 1500),
+                   'Legendary': Wearable("Legendary", 2, 2, 4, 3, 2, 8, 10, 0.51, 0.07, 1500),
                    'Lava': Wearable("Lava", 2, 2, 3, 2, 3, 10, 10, 0.01, 0.01, 150),
-                   'Mercenary': Wearable("Lava", 2, 2, 3, 2, 3, 10, 10, 0.01, 0.01, 150),
+                   'Mercenary': Wearable("Mercenary", 0, 0, 0, 0, 1, 6, 0, 0, 0, 100),
                    'Sparta': Wearable("Sparta", 2, 0, 0, 2, 2, 20, 20, 0.24, 0.03, 150),
                    'Zodiac': Wearable("Zodiac", 5, 5, 5, 5, 5, 20, 20, 0.48, 0.08, 600),
                    'Viking': Wearable("Viking", 4, 3, 6, 6, 2, 22, 22, 0.29, 0.06, 200),
@@ -13,10 +13,10 @@ dualist_helmetS = {'Default': Wearable(),
                    }
 
 dualist_shoulderS = {'Default': Wearable(),
-                     'Mercenary': Wearable("Lava", 2, 2, 3, 2, 3, 10, 10, 0.01, 0.01, 150),
+                     'Mercenary': Wearable("Mercenary", 0, 0, 0, 0, 1, 0, 3, 0, 0, 0),
                      'Sparta': Wearable("Sparta", 1, 0, 0, 2, 1, 15, 15, 0.23, 0.02, 80),
                      'Lava': Wearable("Lava", 1, 2, 1, 3, 2, 15, 15, 0.11, 0.01, 80),
-                     'Legendary': Wearable("Legendary", 1, 3, 2, 2, 2, 3, 2, 0.56, 0.06, 1100),
+                     'Legendary': Wearable("Legendary", 1, 2, 4, 2, 2, 3, 2, 0.55, 0.06, 1100),
                      'Warrior': Wearable("Warrior", 6, 5, 8, 8, 2, 8, 8, 0.44, 0.08, 200),
                      'Viking': Wearable("Viking", 4, 3, 6, 6, 2, 22, 22, 0.29, 0.06, 200),
                      'Zodiac': Wearable("Zodiac", 5, 5, 5, 5, 5, 15, 15, 0.48, 0.08, 600),
@@ -24,10 +24,10 @@ dualist_shoulderS = {'Default': Wearable(),
                      }
 
 dualist_armorS = {'Default': Wearable(),
-                  'Mercenary': Wearable("Lava", 2, 2, 3, 2, 3, 10, 10, 0.01, 0.01, 150),
+                  'Mercenary': Wearable("Mercenary", 0, 0, 0, 0, 0, 5, 5, 0, 0, 0),
                   'Sparta': Wearable("Sparta", 1, 0, 0, 2, 2, 20, 20, 0.24, 0.03, 150),
                   'Lava': Wearable("Lava", 2, 1, 2, 2, 3, 10, 10, 0.12, 0.01, 150),
-                  'Legendary': Wearable("Legendary", 1, 3, 4, 1, 2, 12, 11, 0.67, 0.07, 2000),
+                  'Legendary': Wearable("Legendary", 1, 2, 4, 1, 2, 12, 11, 0.66, 0.07, 2000),
                   'Warrior': Wearable("Warrior", 6, 5, 8, 8, 2, 12, 12, 0.48, 0.06, 200),
                   'Viking': Wearable("Viking", 4, 3, 6, 6, 2, 22, 22, 0.29, 0.06, 200),
                   'Zodiac': Wearable("Zodiac", 5, 5, 5, 5, 5, 23, 23, 0.48, 0.08, 600),
@@ -35,34 +35,34 @@ dualist_armorS = {'Default': Wearable(),
 
 dualist_armS = {'Default': Wearable(),
                 'Sparta': Wearable("Sparta", 2, 0, 0, 1, 1, 10, 10, 0.18, 0.01, 150),
-                'Mercenary': Wearable("Lava", 2, 2, 3, 2, 3, 10, 10, 0.01, 0.01, 150),
+                'Mercenary': Wearable("Mercenary", 0, 0, 0, 0, 1, 0, 2, 0, 0, 0),
                 'Lava': Wearable("Lava", 2, 2, 1, 1, 1, 10, 10, 0.09, 0.05, 150),
-                'Legendary': Wearable("Legendary", 1, 3, 1, 1, 2, 2, 3, 0.53, 0.05, 1200),
+                'Legendary': Wearable("Legendary", 1, 1, 3, 1, 2, 2, 3, 0.46, 0.05, 1200),
                 'Warrior': Wearable("Warrior", 6, 5, 8, 8, 2, 8, 8, 0.42, 0.11, 200),
                 'Viking': Wearable("Viking", 4, 3, 6, 6, 2, 22, 22, 0.29, 0.06, 200),
                 'Zodiac': Wearable("Zodiac", 5, 5, 5, 5, 5, 15, 15, 0.48, 0.08, 600),
                 'Pioneer Viking': Wearable("Pioneer Viking", 6, 5, 5, 5, 1, 22, 18, 0.44, 0.1, 300)}
 
 dualist_meleeS = {'Default': Wearable(),
-                  'Mercenary': Wearable("Lava", 2, 2, 3, 2, 3, 10, 10, 0.01, 0.01, 150),
+                  'Mercenary': Wearable("Mercenary", 1, 1, 1, 1, 0, 0, 0, 0, 0, 0),
                   'Sparta': Wearable("Sparta", 6, 0, 0, 3, 0, 15, 15, 0.42, 0.04, 150),
                   'Lava': Wearable("Lava", 4, 4, 4, 4, 4, 7, 7, 0.22, 0.01, 150),
-                  'Legendary': Wearable("Legendary", 4, 5, 5, 6, 2, 0, 0, 0.86, 0.07, 600),
-                  'Warrior': Wearable("Warrior", 5, 11, 9, 8, 3, 4, 4, 1.22, 0.13, 300),
-                  'Viking': Wearable("Viking", 4, 9, 8, 9, 3, 12, 12, 1.08, 0.09, 300),
-                  'Zodiac': Wearable("Zodiac", 3, 11, 7, 3, 8, 2, 2, 0.89, 0.1, 700),
-                  'Pioneer Viking': Wearable("Pioneer Viking", 6, 14, 6, 7, 2, 12, 10, 1.62, 0.14, 500),
+                  'Legendary': Wearable("Legendary", 5, 5, 5, 6, 2, 0, 0, 0.86, 0.07, 600),
+                  'Warrior': Wearable("Warrior", 12, 2, 12, 6, 3, 7, 7, 1.02, 0.12, 200),
+                  'Viking': Wearable("Viking", 9, 2, 11, 5, 3, 12, 12, 0.45, 0.05, 200),
+                  'Zodiac': Wearable("Zodiac", 10, 3, 10, 3, 8, 2, 2, 0.74, 0.09, 500),
+                  'Pioneer Viking': Wearable("Pioneer Viking", 13, 3, 9, 4, 2, 12, 10, 0.68, 0.08, 300),
                   }
 
 dualist_rangeS = {'Default': Wearable(),
-                  'Mercenary': Wearable("Lava", 2, 2, 3, 2, 3, 10, 10, 0.01, 0.01, 150),
+                  'Mercenary': Wearable("Mercenary", 1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
                   'Sparta': Wearable("Sparta", 4, 0, 0, 2, 1, 10, 10, 0.24, 0.03, 150),
                   'Lava': Wearable("Lava", 2, 2, 2, 2, 2, 5, 5, 0.09, 0.01, 150),
-                  'Legendary': Wearable("Legendary", 4, 5, 3, 3, 2, 1, 1, 0.71, 0.06, 600),
-                  'Warrior': Wearable("Warrior", 2, 11, 8, 5, 3, 3, 3, 1.02, 0.12, 150),
-                  'Viking': Wearable("Viking", 3, 9, 1, 9, 2, 10, 10, 0.09, 0.09, 150),
-                  'Zodiac': Wearable("Zodiac", 2, 9, 5, 3, 7, 1, 1, 0.73, 0.09, 400),
-                  'Pioneer Viking': Wearable("Pioneer Viking", 4, 14, 6, 6, 1, 10, 8, 1.35, 0.13, 200)
+                  'Legendary': Wearable("Legendary", 4, 4, 5, 3, 2, 1, 1, 0.71, 0.06, 600),
+                  'Warrior': Wearable("Warrior", 8, 2, 7, 9, 2, 7, 7, 1.22, 0.13, 200),
+                  'Viking': Wearable("Viking", 5, 2, 6, 8, 2, 10, 10, 0.74, 0.08, 200),
+                  'Zodiac': Wearable("Zodiac", 6, 5, 5, 6, 7, 1, 1, 0.95, 0.13, 500),
+                  'Pioneer Viking': Wearable("Pioneer Viking", 7, 3, 5, 6, 1, 10, 8, 1.11, 0.12, 300)
                   }
 
 
--- main/source.py
+++ main/source.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+from engine.roles.archer import Archer
+from engine.roles.dualist import Dualist
 from engine.roles.mage import Mage
 from engine.roles.blacksmith import Blacksmith
 from engine.roles.swordman import Swordman
@@ -24,6 +26,10 @@ archer_png = PhotoImage(file="img/archer.png")
 ninja_png = PhotoImage(file="img/ninja.png")
 dualist_png = PhotoImage(file="img/dualist.png")
 
+# TODO (BLACKSMITH, SWORDMAN) CLOTHES
+# TODO I NEED DUALIST ADVANCED BONUS STATS
+# DONE ARCHER, MAGE, DUALIST, NINJA, ARCHER
+
 Button(choose_window, image=blacksmith_png,
        command=lambda: displayCalculator(Blacksmith(), title_blacksmith, choose_window)).pack()
 
@@ -33,11 +39,13 @@ Button(choose_window, image=swordman_png,
 Button(choose_window, image=mage_png,
        command=lambda: displayCalculator(Mage(), title_mage, choose_window)).pack()
 
-Button(choose_window, image=archer_png).pack()
+Button(choose_window, image=archer_png,
+       command=lambda: displayCalculator(Archer(), title_mage, choose_window)).pack()
 
 Button(choose_window, image=ninja_png,
        command=lambda: displayCalculator(Ninja(), title_ninja, choose_window)).pack()
 
-Button(choose_window, image=dualist_png).pack()
+Button(choose_window, image=dualist_png,
+       command=lambda: displayCalculator(Dualist(), title_mage, choose_window)).pack()
 
 choose_window.mainloop()
